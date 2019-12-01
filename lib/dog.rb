536ddb045300d92c668eb35fd71afdaabc296067
Antony Sanders <antony.devon92@gmail.com>
2019-12-01 04:44:54 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog 
+  
+  def initialize(name, breed)
+    @dogs_name, @dogs_breed = name, breed
+  end
+  
+end
+
+
+
+
+
+=begin
+
+class Fred
+  def initialize(p1, p2)
+    @a, @b = p1, p2
+  end
+end
+fred = Fred.new('cat', 99)
+fred.instance_variable_get(:@a)    #=> "cat"
+fred.instance_variable_get("@b")   #=> 99
+
+=end
